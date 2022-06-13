9b7524988a6e2b8842331cd8db5e79946b4d78a9
Abdessamad Pas <boughadoin@gmail.com>
2022-06-13 11:43:33 +0100

    new commit

--- firebase-config.js
+++ firebase-config.js
@@ -1,18 +1,21 @@
 // Import the functions you need from the SDKs you need
 
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore, setDoc, doc } from 'firebase/firestore';
-
 
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
+ export const firebaseConfig = {
   apiKey: "AIzaSyB9MuQ8eRxecLiH9sAsUNn1ly-hom1yyEE",
   authDomain: "test-login-1f731.firebaseapp.com",
+  databaseURL: "https://test-login-1f731-default-rtdb.firebaseio.com",
   projectId: "test-login-1f731",
   storageBucket: "test-login-1f731.appspot.com",
   messagingSenderId: "427110507586",
@@ -23,11 +26,7 @@ const firebaseConfig = {
 
 
 
-// Initialize Firebase
-
-
 const app = initializeApp(firebaseConfig);
-
-
 export const auth = getAuth(app);;
 export const dbFirestore = getFirestore(app);
+export { firebase }
